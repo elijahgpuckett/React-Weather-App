758f4154e70d626a62ada34c9e221f1c6f4e09ff
Elijah Puckett <62103502+elijahgpuckett@users.noreply.github.com>
2020-04-24 16:52:53 -0400

    Updated App

--- App.js
+++ App.js
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 const api = {
-  key: "28802535343b2c4cb27043e536734e4c",
-  base: "https://api.openweathermap.org/data/2.5/",
+  key: 'ee194a6612330e435d3e2d9f9147dbf2',
+  base: "http://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result)
+          setQuery('');
+          console.log(result)
+          console.log(weather)
+        })
+    }
+  }
+
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className="app">
       <main>
         <div className="search">
-          <input type="text" className="searchBar" placeholder="Search.." />
-          <button type="button" className="button">
-            Search
-          </button>
+          <input type="text" className="searchBar" placeholder="Where are we looking?"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-      </main>
 
-      <div className="locationResults">
-        <div className="location"></div>
-        <div className="date"></div>
-      </div>
+        {(typeof weather.main != "undefined") ? (
+          <div className="allResults">
+            <div className="locationResults">
+              <div className="location">{weather.name}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weatherBox">
+              <div className="temp">{Math.round(weather.main.temp)} Degrees</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ('')}
+      </main>
     </div>
+
   );
 }
 export default App;
